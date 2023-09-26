59de4bb90ce7aadcc7d0d9251521686cc8629ccc
Zhuraev Donish <zhuraev2003@bk.ru>
2023-09-26 17:53:41 +0300

    commit

--- main.cpp
+++ main.cpp
@@ -3,38 +3,37 @@
 #include "containers/data_base.h"
 #include "dialogue/dialogue.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    if(argc > 2)
-    {
-        std::cout << "Enter the filepath only!\n";
-        return -1;
-    }
-    else if(argc < 2)
-    {
-        std::cout << "Enter the filepath!\n";
-        return -1;
-    }
+//    if(argc > 2)
+//    {
+//        std::cout << "Enter the filepath only!\n";
+//        return -1;
+//    }
+//    else if(argc < 2)
+//    {
+//        std::cout << "Enter the filepath!\n";
+//        return -1;
+//    }
 
     auto *db = new data_base();
 
-    std::string command;
+//    std::string command;
 
-    std::ifstream file(argv[1]);
+//    std::ifstream file(argv[1]);
+//
+//    if (file.is_open())
+//    {
+//        while (std::getline(file, command)) {
+//            db->handle_request(command);
+//        }
+//    }
+//    else
+//    {
+//        std::cout << "File with name:" << argv[1] << " can't be opened" << std::endl;
+//    }
 
-    if (file.is_open())
-    {
-        while (std::getline(file, command)) {
-            db->handle_request(command);
-        }
-    }
-    else
-    {
-        std::cout << "File with name:" << argv[1] << " can't be opened" << std::endl;
-    }
-
-
-
+    dialogue_with_user(db);
 
     delete db;
 
