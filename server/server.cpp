bc10ec15ece8a4c5d1973facf37c18576a02226d
Zhuraev Donish <zhuraev2003@bk.ru>
2023-10-05 21:53:30 +0300

    commit

---
+++ server/server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zhura on 05.10.2023.
+//
+
+#include "server.h"
---
+++ server/server.h
@@ -0,0 +1,12 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "../containers/data_base.h"
+
+class server
+{
+
+};
+
+
+#endif //SERVER_H
