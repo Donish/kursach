952f3e848cf54fbd1fd20598d64877106d4c8300
Zhuraev Donish <zhuraev2003@bk.ru>
2023-10-06 22:23:24 +0300

    commit

--- server/server.cpp
+++ server/server.cpp
@@ -1,2 +1,13 @@
-#include "server.h"
+#include <iostream>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
 
+#include "../containers/data_base.h"
+
+void message_queues()
+{
+
+}
+
+int main()
