eb49f49f1fd53c4b1644f30a2a5e1a628b2fbaf3
Zhuraev Donish <zhuraev2003@bk.ru>
2023-10-17 14:38:07 +0300

    commit

---
+++ cmake-build-debug/clients/CMakeFiles/client.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/zhura/CLionProjects/kursach/backup_system/backup_system.cpp" "clients/CMakeFiles/client.dir/__/backup_system/backup_system.cpp.o" "gcc" "clients/CMakeFiles/client.dir/__/backup_system/backup_system.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/string_methods/mystring.cpp" "clients/CMakeFiles/client.dir/__/string_methods/mystring.cpp.o" "gcc" "clients/CMakeFiles/client.dir/__/string_methods/mystring.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/clients/client.cpp" "clients/CMakeFiles/client.dir/client.cpp.o" "gcc" "clients/CMakeFiles/client.dir/client.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/server/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,59 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/zhura/CLionProjects/kursach/allocator/borders_descriptors_memory.cpp" "server/CMakeFiles/server.dir/__/allocator/borders_descriptors_memory.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/allocator/borders_descriptors_memory.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/allocator/buddy_system_memory.cpp" "server/CMakeFiles/server.dir/__/allocator/buddy_system_memory.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/allocator/buddy_system_memory.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/allocator/memory.cpp" "server/CMakeFiles/server.dir/__/allocator/memory.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/allocator/memory.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/allocator/memory_holder.cpp" "server/CMakeFiles/server.dir/__/allocator/memory_holder.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/allocator/memory_holder.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/allocator/memory_simple.cpp" "server/CMakeFiles/server.dir/__/allocator/memory_simple.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/allocator/memory_simple.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/allocator/sorted_list_memory.cpp" "server/CMakeFiles/server.dir/__/allocator/sorted_list_memory.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/allocator/sorted_list_memory.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command.cpp" "server/CMakeFiles/server.dir/__/command_handler/command.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_add_collection.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_add_collection.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_add_collection.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_add_data.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_add_data.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_add_data.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_add_pool.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_add_pool.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_add_pool.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_add_scheme.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_add_scheme.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_add_scheme.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_get_data.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_get_data.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_get_data.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_get_data_between.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_get_data_between.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_get_data_between.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_remove_collection.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_remove_collection.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_remove_collection.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_remove_data.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_remove_data.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_remove_data.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_remove_pool.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_remove_pool.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_remove_pool.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_remove_scheme.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_remove_scheme.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_remove_scheme.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/command_update_data.cpp" "server/CMakeFiles/server.dir/__/command_handler/command_update_data.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/command_update_data.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/handler_with_command.cpp" "server/CMakeFiles/server.dir/__/command_handler/handler_with_command.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/handler_with_command.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/command_handler/handler_with_command_chain.cpp" "server/CMakeFiles/server.dir/__/command_handler/handler_with_command_chain.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/command_handler/handler_with_command_chain.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/comparers/key_comparer.cpp" "server/CMakeFiles/server.dir/__/comparers/key_comparer.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/comparers/key_comparer.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/comparers/string_comparer.cpp" "server/CMakeFiles/server.dir/__/comparers/string_comparer.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/comparers/string_comparer.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/containers/collection_data.cpp" "server/CMakeFiles/server.dir/__/containers/collection_data.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/containers/collection_data.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/containers/data_base.cpp" "server/CMakeFiles/server.dir/__/containers/data_base.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/containers/data_base.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/containers/pool_data.cpp" "server/CMakeFiles/server.dir/__/containers/pool_data.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/containers/pool_data.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/containers/scheme_data.cpp" "server/CMakeFiles/server.dir/__/containers/scheme_data.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/containers/scheme_data.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/dialogue/dialogue.cpp" "server/CMakeFiles/server.dir/__/dialogue/dialogue.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/dialogue/dialogue.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/flyweight/flyweight_factory.cpp" "server/CMakeFiles/server.dir/__/flyweight/flyweight_factory.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/flyweight/flyweight_factory.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/flyweight/flyweight_string.cpp" "server/CMakeFiles/server.dir/__/flyweight/flyweight_string.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/flyweight/flyweight_string.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/ipc_connections/message_queue.cpp" "server/CMakeFiles/server.dir/__/ipc_connections/message_queue.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/ipc_connections/message_queue.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/logger/logger.cpp" "server/CMakeFiles/server.dir/__/logger/logger.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/logger/logger.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/logger/logger_builder.cpp" "server/CMakeFiles/server.dir/__/logger/logger_builder.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/logger/logger_builder.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/logger/logger_builder_concrete.cpp" "server/CMakeFiles/server.dir/__/logger/logger_builder_concrete.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/logger/logger_builder_concrete.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/logger/logger_concrete.cpp" "server/CMakeFiles/server.dir/__/logger/logger_concrete.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/logger/logger_concrete.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/logger/logger_holder.cpp" "server/CMakeFiles/server.dir/__/logger/logger_holder.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/logger/logger_holder.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/owntype/own_type.cpp" "server/CMakeFiles/server.dir/__/owntype/own_type.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/owntype/own_type.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/string_methods/mystring.cpp" "server/CMakeFiles/server.dir/__/string_methods/mystring.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/string_methods/mystring.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/validator/date_validator.cpp" "server/CMakeFiles/server.dir/__/validator/date_validator.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/validator/date_validator.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/validator/digit_validator.cpp" "server/CMakeFiles/server.dir/__/validator/digit_validator.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/validator/digit_validator.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/validator/time_validator.cpp" "server/CMakeFiles/server.dir/__/validator/time_validator.cpp.o" "gcc" "server/CMakeFiles/server.dir/__/validator/time_validator.cpp.o.d"
+  "/mnt/c/Users/zhura/CLionProjects/kursach/server/server.cpp" "server/CMakeFiles/server.dir/server.cpp.o" "gcc" "server/CMakeFiles/server.dir/server.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
